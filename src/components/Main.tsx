1c252adaa7ea6939aac73cbaec3b3279537fc59c
KNNadeera <nadeera.n@digitustecglobal.com>
2024-08-11 22:01:54 +0530

    added bg image

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,6 @@
 import React, { Children, ReactNode } from "react";
 import Header from "./header/Header";
-import '../utils/scss/main.scss'
+import "../utils/scss/main.scss";
 
 interface IProp {
   children?: ReactNode;
@@ -10,14 +10,14 @@ const Main = ({ children }: IProp) => {
   return (
     <>
       <div className="row main">
-        <div className="col-2">dsfdghfgjkhjlk;</div>
-        <div className="col-8">
+        <div className="col-2" />
+        <div className="col-8 content mt-1 pt-3" >
           <Header />
           {Children.map(children, (child, index) => (
             <div key={index}>{child}</div>
           ))}
         </div>
-        <div className="col-2">dfgdfhgjk</div>
+        <div className="col-2" />
       </div>
     </>
   );
